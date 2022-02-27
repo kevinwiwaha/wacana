5aa7dc0d19828fde30631490809d8664b116ef7e
kevinwiwaha <kevinwiwaha@gmail.com>
2022-02-27 13:57:42 +0700

    update landing page

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ export default function Home() {
   const particlesInit = (main) => {
     console.log(main);
 
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
   };
 
   const particlesLoaded = (container) => {
