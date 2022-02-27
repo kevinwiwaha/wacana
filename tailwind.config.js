4fbcd01d5cc1f6487c2f9d25d3360d38d441cf0a
tiketdatakevinw <kevin.wiwaha@tiket.com>
2022-02-27 10:45:19 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,56 @@
+const colors = require('tailwindcss/colors')
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./styles/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      red: colors.red,
+      yellow: colors.yellow,
+      'wacana': {
+        700: '#1E6561',
+        800: '#154744',
+        900: '#0B2423'
+      }
+
+
+      // ...
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#154744",
+
+          "secondary": "#F000B8",
+
+          "accent": "#37CDBE",
+
+          "neutral": "#2A2E37",
+
+          "base-100": "#F9F7F2",
+
+          "info": "#3ABFF8",
+
+          "success": "#36D399",
+
+          "warning": "#FBBD23",
+
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
+}
